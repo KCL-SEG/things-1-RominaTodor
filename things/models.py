5d9b761a3086d61ebf53e07b7c1e546ddc1a5e4d
RominaTodor <romina.todor@kcl.ac.uk>
2023-12-08 09:54:47 +0000

    Add advanced attributes to Thing entity

--- things/models.py
+++ things/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Thing(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    quantity = models.IntegerField()
+    # name must be unique, must not be blank, and must consist of 30 characters or less.
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    # description need not be unique, may be blank, and must consist of 120 characters of less.
+    description = models.TextField(max_length=120)
+    # quantity need not be unique, and must be an integer value between 0 and 100 (inclusive). quantity may be 0 and it may be 100, but not less than 0 and not more than 100.
+    quantity = models.PositiveIntegerField(max_value=100)
