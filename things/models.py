23b98fc36d6100d357f79661636fbd1bd3c51224
RominaTodor <romina.todor@kcl.ac.uk>
2023-12-08 10:57:09 +0000

    Add advanced attributes to Thing entity with new import

--- things/models.py
+++ things/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 class Thing(models.Model):
     # name must be unique, must not be blank, and must consist of 30 characters or less.
@@ -6,4 +7,5 @@ class Thing(models.Model):
     # description need not be unique, may be blank, and must consist of 120 characters of less.
     description = models.TextField(max_length=120)
     # quantity need not be unique, and must be an integer value between 0 and 100 (inclusive). quantity may be 0 and it may be 100, but not less than 0 and not more than 100.
-    quantity = models.PositiveIntegerField(max_value=100)
+    quantity = models.PositiveIntegerField(validators=[MaxValueValidator(100)]
+    )
